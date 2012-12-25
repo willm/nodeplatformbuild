4849cd9b565612f491c49c6ffe726e39b4b6a1ab
Will Munn <willmunn@hotmail.com>
2012-12-25 15:55:51 +0000

    added logging

--- updateService.js
+++ updateService.js
@@ -1,15 +1,18 @@
 var fs = require('fs'),
 	git = require('./git'),
-	path = require('path');
+	path = require('path'),
+	color = require('cli-color');
 
 exports.update = function(project){
 	var startDirectory = process.cwd();
 	process.chdir(path.join(startDirectory, project.path));
 
 	git.branch(function(branch){
+		console.log(color.cyan("Updating " + startDirectory + " to latest " + branch));
 		git.status(function(changes){
 			if(changes){
 				git.stash.save(function(){
+				console.log(color.blue('Changes will be stashed and re-applied'));
 					git.pull(branch, function(){
 						git.stash.pop();
 					});
