e437049a54ff130c2b2a17ba8558a3a284388e56
Will Munn <willmunn@hotmail.com>
2012-12-27 11:59:59 +0000

    working git clone int test

--- specs/integration/git.spec.js
+++ specs/integration/git.spec.js
@@ -1,19 +1,33 @@
 var git = require('../../git.js'),
 	path = require('path'),
-	fs = require('fs');
+	fs = require('fs-extra');
 
 describe("git", function() {
 	var testDir = path.join(process.cwd(),'test');
-	afterEach(function(){
-		fs.rmdir(testDir);
-	});
+	var removeTest = function(){
+		fs.removeSync(testDir);
+	};
+
+	beforeEach(removeTest);
+
+	afterEach(removeTest);
 
 	it("should clone", function() {
+		var complete = false;
 		fs.mkdirSync(testDir);
 
-		git.clone('git@github.com:willm/WebHelper.git',testDir,function(){
-			expect(fs.existsSync('src')).toBe(true);
-			fs.rmdir(testDir);
+		runs(function(){
+			git.clone('git@github.com:willm/WebHelper.git',testDir,function(){
+				complete = true;
+			});
+		});
+		
+		waitsFor(function(){
+			return complete;
+		}, 20000);
+
+		runs(function(){
+			expect(fs.existsSync(path.join(testDir,'src'))).toBe(true);
 		});
 	});
 });
