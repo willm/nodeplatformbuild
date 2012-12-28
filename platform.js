e3adc11805251348041414fe476c039a916816f9
Will <will@willubuntu.(none)>
2012-12-28 17:19:54 +0000

    pass git repo through to updater

--- platform.js
+++ platform.js
@@ -1,12 +1,13 @@
 var fs = require('fs'),
 	updateService = require('./updateService'),
 	cloner = require('./cloner'),
-	path = require('path');
+	path = require('path'),
+	git = require('./git.js');
 
 exports.syncProject = function (project, cb) {
 	if (!fs.existsSync(path.join(project.path, '.git'))) {
 		cloner.clone(project, cb);
 	} else {
-		updateService.update(project, cb);
+		updateService.update(git.open(project.path), cb);
 	}
 };
