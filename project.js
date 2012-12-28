b40bdbf52b5f2bab667dbd5b0dc31d1ff90717d4
Will <will@willubuntu.(none)>
2012-12-28 17:26:49 +0000

    removed duplicate file

--- project.js
+++ project.js
@@ -1,13 +1,14 @@
 var fs = require('fs'),
 	updateService = require('./updateService'),
 	cloner = require('./cloner'),
-	path = require('path');
+	path = require('path'),
+	git = require('./git');
 
 exports.syncProject = function (project, cb) {
 	if (!fs.existsSync(path.join(project.path, '.git'))) {
 		cloner.clone(project, cb);
 	} else {
-		updateService.update(project, cb);
+		updateService.update(git.open(project, cb));
 	}
 };
 
