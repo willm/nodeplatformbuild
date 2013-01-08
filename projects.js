7f029c3ecf2e18a61977cd188837bef42b7f59a6
Will Munn <willmunn@hotmail.com>
2013-01-08 19:31:22 +0000

    get projects from rules file

--- projects.js
+++ projects.js
@@ -1,4 +1,24 @@
-var Project = require('./project');
+var fs = require('fs'),
+	os = require('os'),
+	Project = require('./project');
+
+var getProjects = function(){
+	var projects = [];
+	var path, url;
+	readAllLines('_rules/Modules.rule').forEach(function(line){
+		if(line !== ''){
+			path = line.substring(0,line.lastIndexOf('=')).trim();
+			url = line.substring(line.lastIndexOf('=') + 1).trim();
+			projects.push(new Project(par, url));
+		}
+	});
+	return projects;
+}
+
+var readAllLines = function(path){
+	return fs.readFileSync(path,'utf8').split(os.EOL);
+}
 
 exports.getAll = function(){
+	return getProjects
 }
